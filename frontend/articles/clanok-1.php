1396c769ab7913122495cb02f039558ef4513525
RichardKovacik <56000073+RichardKovacik@users.noreply.github.com>
2022-10-09 19:38:02 +0200

    Pridany clanok-1.php

--- frontend/articles/clanok-1.php
+++ frontend/articles/clanok-1.php
@@ -3,10 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <title>Clanok-1</title>
+    <link rel="stylesheet" href="clanok1.css">
+    <script src="../utilities/nav-bar-js.js"></script>
 </head>
 <header>
     <?php include '../utilities/nav-bar.php';?>
 </header>
 <body>
+<article class="article">
+    <h1>Postup este nie je strateny</h1>
+    <img src="../resources/slovakia-fotball.avif" class="obrazok" alt="Obrazok">
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam consequat lobortis ullamcorper. Phasellus feugiat luctus ipsum, at vehicula neque scelerisque nec. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce sit amet dictum erat. Aenean eget eros non tortor luctus convallis. In hac habitasse platea dictumst. Cras consectetur bibendum magna, nec aliquet felis pretium vitae. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris sodales dolor mauris, et viverra est finibus sed. Mauris malesuada interdum scelerisque. Suspendisse eu sapien facilisis, dictum ligula sit amet, mattis risus. </p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam consequat lobortis ullamcorper. Phasellus feugiat luctus ipsum, at vehicula neque scelerisque nec. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce sit amet dictum erat. Aenean eget eros non tortor luctus convallis. In hac habitasse platea dictumst. Cras consectetur bibendum magna, nec aliquet felis pretium vitae. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris sodales dolor mauris, et viverra est finibus sed. Mauris malesuada interdum scelerisque. Suspendisse eu sapien facilisis, dictum ligula sit amet, mattis risus. </p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam consequat lobortis ullamcorper. Phasellus feugiat luctus ipsum, at vehicula neque scelerisque nec. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce sit amet dictum erat. Aenean eget eros non tortor luctus convallis. In hac habitasse platea dictumst. Cras consectetur bibendum magna, nec aliquet felis pretium vitae. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris sodales dolor mauris, et viverra est finibus sed. Mauris malesuada interdum scelerisque. Suspendisse eu sapien facilisis, dictum ligula sit amet, mattis risus. </p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam consequat lobortis ullamcorper. Phasellus feugiat luctus ipsum, at vehicula neque scelerisque nec. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce sit amet dictum erat. Aenean eget eros non tortor luctus convallis. In hac habitasse platea dictumst. Cras consectetur bibendum magna, nec aliquet felis pretium vitae. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris sodales dolor mauris, et viverra est finibus sed. Mauris malesuada interdum scelerisque. Suspendisse eu sapien facilisis, dictum ligula sit amet, mattis risus. </p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam consequat lobortis ullamcorper. Phasellus feugiat luctus ipsum, at vehicula neque scelerisque nec. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Fusce sit amet dictum erat. Aenean eget eros non tortor luctus convallis. In hac habitasse platea dictumst. Cras consectetur bibendum magna, nec aliquet felis pretium vitae. Interdum et malesuada fames ac ante ipsum primis in faucibus. Mauris sodales dolor mauris, et viverra est finibus sed. Mauris malesuada interdum scelerisque. Suspendisse eu sapien facilisis, dictum ligula sit amet, mattis risus. </p>
+</article>
 </body>
 </html>
